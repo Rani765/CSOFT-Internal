f2bc37ca0855441b57b6f68f7a22a4531f2b8ac1
monobikash <monobikash.d@cloudworkmates.com>
2025-07-03 16:21:56 +0530

    added http api gw and openapi spec file

--- apigw.tf
+++ apigw.tf
@@ -1,3 +1,6 @@
+locals {
+  alb_listener_arn = "arn:aws:elasticloadbalancing:ap-south-1:024848447708:listener/app/Csoft-prod-alb/5fc1dd795c5839cb/b81bf5f7f5678f06"
+}
 module "apigw-vpc-link-securtiy-group" {
   source      = "./modules/sg"
   name        = "csoft-prod-apigw-vpc-link-sg"
@@ -12,4 +15,17 @@ resource "aws_apigatewayv2_vpc_link" "this" {
   security_group_ids = [module.apigw-vpc-link-securtiy-group.security_group_id]
   subnet_ids         = module.vpc.private_subnets
 
+}
+resource "aws_apigatewayv2_api" "this" {
+  depends_on = [aws_apigatewayv2_vpc_link.this]
+
+  name                         = "csoft-prod-wiapi-gt"
+  protocol_type                = "HTTP"
+  description                  = "Csoft prod api gw for wi-api"
+  disable_execute_api_endpoint = false
+  ip_address_type              = "ipv4"
+  body = templatefile("apigw_oas30.yaml", {
+    apigw_vpc_link_id = aws_apigatewayv2_vpc_link.this.id
+    alb_listener_arn  = local.alb_listener_arn
+  })
 }
